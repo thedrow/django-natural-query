7907b3db75d02177288144d403d46e68772c4e18
Omer Katz <omer.drow@gmail.com>
2014-02-24 17:47:46 +0200

    Complete overhaul.

--- natural_query/apps.py
+++ natural_query/apps.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.apps import AppConfig
-from django.db.models import Field
-from django.db.models.loading import get_apps, get_models
-
-from natural_query.mixins import _mixin, NaturalQueryModelMixin, NaturalQueryFieldMixin
 
 
 class NaturalQueryConfig(AppConfig):
     name = 'natural_query'
-
-    def ready(self):
-        _mixin(Field, NaturalQueryFieldMixin)
-
-        for app in get_apps():
-            for model in get_models(app):
-                _mixin(model, NaturalQueryModelMixin)
