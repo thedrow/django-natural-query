c701866d0721d64ae9e83fea30ab9e5f25395188
Omer Katz <omer.drow@gmail.com>
2014-03-09 14:44:39 +0300

    Changed methods to be private.

--- natural_query/query.py
+++ natural_query/query.py
@@ -15,31 +15,31 @@ class NaturalQueryDescriptor(object):
     def __init__(self, name):
         self.name = name
 
-    def transform_operator_to_query_object(self, lookup_type, other):
+    def _transform_operator_to_query_object(self, lookup_type, other):
         other = _get_value_or_field(other)
-        constructed_lookup = self.construct_lookup(lookup_type)
+        constructed_lookup = self._construct_lookup(lookup_type)
         return create_query_object(constructed_lookup, other)
 
-    def construct_lookup(self, lookup_type):
+    def _construct_lookup(self, lookup_type):
         return '%s__%s' % (self.name, lookup_type)
 
     def __eq__(self, other):
-        return self.transform_operator_to_query_object('exact', other)
+        return self._transform_operator_to_query_object('exact', other)
 
     def __gt__(self, other):
-        return self.transform_operator_to_query_object('gt', other)
+        return self._transform_operator_to_query_object('gt', other)
 
     def __ge__(self, other):
-        return self.transform_operator_to_query_object('gte', other)
+        return self._transform_operator_to_query_object('gte', other)
 
     def __lt__(self, other):
-        return self.transform_operator_to_query_object('lt', other)
+        return self._transform_operator_to_query_object('lt', other)
 
     def __le__(self, other):
-        return self.transform_operator_to_query_object('lte', other)
+        return self._transform_operator_to_query_object('lte', other)
 
     def __ne__(self, other):
-        return ~self.transform_operator_to_query_object('exact', other)
+        return ~self._transform_operator_to_query_object('exact', other)
 
     def __invert__(self):
         return ~Q(self.name)
