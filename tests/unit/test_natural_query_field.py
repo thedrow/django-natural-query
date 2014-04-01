0c75353842bd0dcb4f4397e5ce3444fdf49f2e18
Omer Katz <omer.drow@gmail.com>
2014-04-01 15:41:46 +0400

    Made sure the attribute is deleted before adding it back.

--- tests/unit/test_natural_query_field.py
+++ tests/unit/test_natural_query_field.py
@@ -1,4 +1,5 @@
 from django.test import SimpleTestCase
+
 from natural_query.fields import NaturalQueryField
 from natural_query.query import NaturalQueryDescriptor
 from tests.common.support.models import TestModel
@@ -7,6 +8,8 @@ from tests.common.support.models import TestModel
 class NaturalQueryFieldTestCase(SimpleTestCase):
     def test_a_query_descriptor_is_added_to_the_model(self):
         sut = NaturalQueryField(name='foo')
+
+        del TestModel.foo
 
         sut.contribute_to_class(TestModel, 'foo')
 
