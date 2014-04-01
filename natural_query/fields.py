e6c826fff434ca2da28886137a8d7b5b8a10a565
Omer Katz <omer.drow@gmail.com>
2014-04-01 20:26:38 +0400

    Extracting a mixin that contains the common logic for related fields.

--- natural_query/fields.py
+++ natural_query/fields.py
@@ -21,12 +21,14 @@ class DateTimeNaturalQueryField(DateTimeField, NaturalQueryField):
     query_descriptor = DateTimeNaturalQueryDescriptor
 
 
-class ForeignKeyNaturalQueryField(ForeignKey, NaturalQueryField):
-    query_descriptor = NaturalQueryDescriptor
-
+class RelatedFieldNaturalQueryMixin(object):
     def contribute_to_class(self, cls, name, virtual_only=False):
-        super(ForeignKeyNaturalQueryField, self).contribute_to_class(cls, name, virtual_only)
+        super(RelatedFieldNaturalQueryMixin, self).contribute_to_class(cls, name, virtual_only)
 
         relation_id_attribute_name = '%s_id' % name
         if not hasattr(cls, relation_id_attribute_name):
             setattr(cls, relation_id_attribute_name, self.query_descriptor(relation_id_attribute_name))
+
+
+class ForeignKeyNaturalQueryField(ForeignKey, RelatedFieldNaturalQueryMixin):
+    query_descriptor = NaturalQueryDescriptor
