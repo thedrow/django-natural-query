04657ad7d3d137ec65eeba07cd6882e894e110ff
Omer Katz <omer.drow@gmail.com>
2015-03-23 14:32:49 +0200

    Tests now ensure that random order is ignored.

--- tests/unit/support/__init__.py
+++ tests/unit/support/__init__.py
@@ -8,18 +8,20 @@ def assertQObjectsEqual(first, second, msg=None):
 
     are_expressions_exactly_equal = str(first) == str(second)
 
-    are_expressions_equivalent = False
+    are_expressions_equivalent = True
     if not are_expressions_exactly_equal:
-        first.children = sorted(first.children, key=lambda c: c[0][0])
-        second.children = sorted(second.children, key=lambda c: c[0][0])
+        first.children = sorted(first.children, key=lambda c: c[0])
+        second.children = sorted(second.children, key=lambda c: c[0])
 
-        first_node = first.children[0][1]
-        second_node = second.children[0][1]
+        for i in range(len(first.children)):
+            first_node = first.children[i][1]
+            second_node = second.children[i][1]
 
-        try:
-            if first_node.connector == second_node.connector == '+':
-                are_expressions_equivalent = str(first_node.rhs) == str(second_node.lhs) and str(first_node.lhs) == str(second_node.rhs)
-        except AttributeError:
-            pass
+            if first_node == second_node:
+                continue
+            elif first_node.connector == second_node.connector == '+':
+                are_expressions_equivalent = are_expressions_equivalent and str(first_node.rhs) == str(second_node.lhs) and str(first_node.lhs) == str(second_node.rhs)
+            else:
+                are_expressions_equivalent = False
 
     assert (are_expressions_exactly_equal or are_expressions_equivalent) and first.connector == second.connector and first.negated == second.negated, msg
